02e889e7fcc0088488133cfa31f419b27428443c
SpectralGT <atharvsingh1396@gmail.com>
2025-03-24 11:08:57 +0530

    formated the code

--- src/main.ts
+++ src/main.ts
@@ -1,165 +1,196 @@
-import './style.css';
-import * as THREE from 'three';
-import RAPIER from '@dimforge/rapier3d';
-import anime from 'animejs';
+import "./style.css";
+import * as THREE from "three";
+import RAPIER from "@dimforge/rapier3d";
+import anime from "animejs";
 // import { simulationVertexShader, simulationFragmentShader, renderVertexShader, renderFragmentShader } from "./shader.js";
 
 // #region Hero Threejs
 
 // await RAPIER.init() // This line is only needed if using the compat version
-const gravity = new RAPIER.Vector3(0.0, 0.0, 0.0)
-const world = new RAPIER.World(gravity)
+const gravity = new RAPIER.Vector3(0.0, 0.0, 0.0);
+const world = new RAPIER.World(gravity);
 // const dynamicBodies: [THREE.Object3D, RAPIER.RigidBody][] = []
-const dynamicBodies: [THREE.Object3D, RAPIER.RigidBody][] = []
+const dynamicBodies: [THREE.Object3D, RAPIER.RigidBody][] = [];
 
-
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 const ambientLight = new THREE.AmbientLight();
 ambientLight.intensity = 1;
 scene.add(ambientLight);
 
-const light1 = new THREE.DirectionalLight('#ffffff', 2);
-light1.position.set(10, 10, 10)
+const light1 = new THREE.DirectionalLight("#ffffff", 2);
+light1.position.set(10, 10, 10);
 // light1.angle = Math.PI / 3
 // light1.penumbra = 0.5
-light1.castShadow = true
-light1.shadow.blurSamples = 10
-light1.shadow.radius = 5
-scene.add(light1)
+light1.castShadow = true;
+light1.shadow.blurSamples = 10;
+light1.shadow.radius = 5;
+scene.add(light1);
 
-const light2 = light1.clone()
-light2.position.set(10, -1, 10)
+const light2 = light1.clone();
+light2.position.set(10, -1, 10);
 // scene.add(light2)
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(0, 0, 15)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.set(0, 0, 15);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.VSMShadowMap
-document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.VSMShadowMap;
+document.body.appendChild(renderer.domElement);
 
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-})
-
-
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 for (let i = 0; i < 50; i++) {
+  // Ball Collider
+  const sphereMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshStandardMaterial({
+      color: "#ff2222",
+      roughness: 0,
+      metalness: 0,
+    })
+  );
+  sphereMesh.castShadow = true;
+  scene.add(sphereMesh);
 
+  const sphereBody = world.createRigidBody(
+    RAPIER.RigidBodyDesc.dynamic()
+      .setTranslation(i, i, 0)
+      .setCanSleep(false)
+      .setLinearDamping(4)
+      .setAngularDamping(1)
+  );
+  const sphereShape = RAPIER.ColliderDesc.ball(1)
+    .setMass(1)
+    .setRestitution(0.5)
+    .setFriction(100);
 
-    // Ball Collider
-    const sphereMesh = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshStandardMaterial({ color: "#ff2222", roughness: 0, metalness: 0 }))
-    sphereMesh.castShadow = true
-    scene.add(sphereMesh)
-
-    const sphereBody = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic().setTranslation(i, i, 0).setCanSleep(false).setLinearDamping(4).setAngularDamping(1))
-    const sphereShape = RAPIER.ColliderDesc.ball(1).setMass(1).setRestitution(0.5).setFriction(100)
-
-    world.createCollider(sphereShape, sphereBody)
-    dynamicBodies.push([sphereMesh, sphereBody])
-
+  world.createCollider(sphereShape, sphereBody);
+  dynamicBodies.push([sphereMesh, sphereBody]);
 }
 
-const mouseBallBody = world.createRigidBody(RAPIER.RigidBodyDesc.kinematicPositionBased().setTranslation(100, 100, 100))
-const mouseBallShape = RAPIER.ColliderDesc.ball(3).setMass(1).setRestitution(15).setFriction(100)
+const mouseBallBody = world.createRigidBody(
+  RAPIER.RigidBodyDesc.kinematicPositionBased().setTranslation(100, 100, 100)
+);
+const mouseBallShape = RAPIER.ColliderDesc.ball(3)
+  .setMass(1)
+  .setRestitution(15)
+  .setFriction(100);
 
-world.createCollider(mouseBallShape, mouseBallBody)
-
+world.createCollider(mouseBallShape, mouseBallBody);
 
 // const stats = new Stats()
 // document.body.appendChild(stats.dom)
 
-
-const clock = new THREE.Clock()
-let delta
+const clock = new THREE.Clock();
+let delta;
 
 var timeout: any;
 
-window.document.addEventListener('mousemove', (event) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () { mouseBallBody.setTranslation(new RAPIER.Vector3(100, 100, 100), false); }, 10);
+window.document.addEventListener("mousemove", (event) => {
+  clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    mouseBallBody.setTranslation(new RAPIER.Vector3(100, 100, 100), false);
+  }, 10);
 
-    var vec = new THREE.Vector3(); // create once and reuse
-    var pos = new THREE.Vector3(); // create once and reuse
+  var vec = new THREE.Vector3(); // create once and reuse
+  var pos = new THREE.Vector3(); // create once and reuse
 
-    vec.set(
-        (event.clientX / window.innerWidth) * 2 - 1,
-        - (event.clientY / window.innerHeight) * 2 + 1,
-        0.5,
-    );
+  vec.set(
+    (event.clientX / window.innerWidth) * 2 - 1,
+    -(event.clientY / window.innerHeight) * 2 + 1,
+    0.5
+  );
 
-    vec.unproject(camera);
+  vec.unproject(camera);
 
-    vec.sub(camera.position).normalize();
+  vec.sub(camera.position).normalize();
 
-    var distance = - camera.position.z / vec.z;
+  var distance = -camera.position.z / vec.z;
 
-    pos.copy(camera.position).add(vec.multiplyScalar(distance));
+  pos.copy(camera.position).add(vec.multiplyScalar(distance));
 
-    mouseBallBody.setTranslation(new RAPIER.Vector3(pos.x, pos.y, 0), true);
-
-
-})
+  mouseBallBody.setTranslation(new RAPIER.Vector3(pos.x, pos.y, 0), true);
+});
 
 function animate() {
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
 
-    delta = clock.getDelta()
-    world.timestep = Math.min(delta, 0.1)
-    world.step()
+  delta = clock.getDelta();
+  world.timestep = Math.min(delta, 0.1);
+  world.step();
 
-    let multiplier = 0.2;
-    dynamicBodies.forEach((b) => {
-        b[1].applyImpulse(new RAPIER.Vector3(-b[0].position.x * multiplier, -b[0].position.y * multiplier, -b[0].position.z * multiplier), true)
-    })
+  let multiplier = 0.2;
+  dynamicBodies.forEach((b) => {
+    b[1].applyImpulse(
+      new RAPIER.Vector3(
+        -b[0].position.x * multiplier,
+        -b[0].position.y * multiplier,
+        -b[0].position.z * multiplier
+      ),
+      true
+    );
+  });
 
-    for (let i = 0, n = dynamicBodies.length; i < n; i++) {
-        dynamicBodies[i][0].position.copy(dynamicBodies[i][1].translation())
-        dynamicBodies[i][0].quaternion.copy(dynamicBodies[i][1].rotation())
-    }
+  for (let i = 0, n = dynamicBodies.length; i < n; i++) {
+    dynamicBodies[i][0].position.copy(dynamicBodies[i][1].translation());
+    dynamicBodies[i][0].quaternion.copy(dynamicBodies[i][1].rotation());
+  }
 
-    renderer.render(scene, camera)
+  renderer.render(scene, camera);
 
-    // stats.update()
+  // stats.update()
 }
 
-animate()
+animate();
 
 // #endregion
 
 // #region TextAnimation
 
-let div = document.querySelector('#name');
-div!.innerHTML = div!.textContent!.replace(/\S/g, "<span class='letter' style='display: inline-block'>$&</span>");
+let div = document.querySelector("#name");
+div!.innerHTML = div!.textContent!.replace(
+  /\S/g,
+  "<span class='letter' style='display: inline-block'>$&</span>"
+);
 
-let div2 = document.querySelector('#role');
-div2!.innerHTML = div2!.textContent!.replace(/\S/g, "<span class='letter' style='display: inline-block'>$&</span>");
+let div2 = document.querySelector("#role");
+div2!.innerHTML = div2!.textContent!.replace(
+  /\S/g,
+  "<span class='letter' style='display: inline-block'>$&</span>"
+);
 
 let timeline = anime.timeline({
-    loop: false,
-    autoplay: true
+  loop: false,
+  autoplay: true,
 });
 
 timeline.add({
-    duration: 1000,
-    delay: (_el, i) => 70 * i,
-    endDelay: 500
-})
+  duration: 1000,
+  delay: (_el, i) => 70 * i,
+  endDelay: 500,
+});
 
 timeline.add({
-    targets: 'h1 .letter',
-    scale: [4, 1],
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: (_el, i) => 70 * i,
-    endDelay: 500
-})
+  targets: "h1 .letter",
+  scale: [4, 1],
+  opacity: [0, 1],
+  easing: "easeOutExpo",
+  duration: 1000,
+  delay: (_el, i) => 70 * i,
+  endDelay: 500,
+});
 
 // #endregion
 
@@ -170,10 +201,8 @@ timeline.add({
 // const scene2 = new THREE.Scene();
 // const simScene = new THREE.Scene();
 
-
 // const camera2 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 // // });
-
 
 // // @ts-ignore
 // const renderer2 = new THREE.WebGLRenderer({
@@ -201,7 +230,6 @@ timeline.add({
 //     depthBuffer: false,
 // };
 
-
 // let rtA = new THREE.WebGLRenderTarget(width, height, options);
 // let rtB = new THREE.WebGLRenderTarget(width, height, options);
 
@@ -217,7 +245,6 @@ timeline.add({
 //     fragmentShader: simulationFragmentShader,
 // });
 
-
 // const renderMaterial = new THREE.ShaderMaterial({
 //     uniforms: {
 //         textureA: { value: null },
@@ -227,9 +254,6 @@ timeline.add({
 //     fragmentShader: renderFragmentShader,
 //     transparent: true,
 // });
-
-
-
 
 // const plane = new THREE.PlaneGeometry(2, 2);
 // const simQuad = new THREE.Mesh(plane, simMaterial);
@@ -247,7 +271,6 @@ timeline.add({
 // ctx!.fillStyle = '#fb7427';
 // ctx!.fillRect(0, 0, width, height)
 
-
 // const fontSize = Math.round(250 * window.devicePixelRatio);
 // ctx!.fillStyle = '#fef4b8';
 // ctx!.font = `bold ${fontSize}px Test Roboto`;
@@ -262,7 +285,6 @@ timeline.add({
 // textTexture.minFilter = THREE.LinearFilter;
 // textTexture.magFilter = THREE.LinearFilter;
 // textTexture.format = THREE.RGBAFormat;
-
 
 // window.addEventListener('resize', () => {
 //     const newWidth = window.innerWidth * window.devicePixelRatio;
@@ -289,17 +311,14 @@ timeline.add({
 
 // })
 
-
 // renderer2.domElement.addEventListener('mousemove', (e) => {
 //     mouse.x = e.clientX * window.devicePixelRatio;
 //     mouse.y = (window.innerHeight - e.clientX) * window.devicePixelRatio;
 // })
 
-
 // renderer2.domElement.addEventListener("mouseleave", () => {
 //     mouse.set(0, 0);
 // })
-
 
 // const animate2 = () => {
 //     simMaterial.uniforms.frame.value = frame++;
@@ -322,6 +341,4 @@ timeline.add({
 //     animate2();
 // };
 
-
 // endregion
-
