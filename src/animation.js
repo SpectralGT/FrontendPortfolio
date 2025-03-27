95dc34d209add5ce47c66034f3bd9d00b947eacc
SpectralGT <atharvsingh1396@gmail.com>
2025-03-27 13:58:22 +0530

    added skills sections animation

--- src/animation.js
+++ src/animation.js
@@ -1,15 +1,11 @@
-import {
-  animate,
-  inView,
-} from "https://cdn.jsdelivr.net/npm/motion@12.6.0/+esm";
-import { delay } from "motion";
+import { animate, inView, stagger } from "motion";
 
 inView("section h2", (element) => {
   animate(
     element,
     { opacity: 1, x: [-100, 0] },
     {
-      delay: 0.1,
+      type: "spring",
       duration: 0.9,
       easing: [0.17, 0.55, 0.55, 1],
     }
@@ -18,4 +14,16 @@ inView("section h2", (element) => {
   return () => animate(element, { opacity: 0, x: -100 });
 });
 
-console.log("hi");
+inView(".skills li", (element) => {
+  animate(
+    ".skills li",
+    { opacity: [0, 1], x: [-100, 0] },
+    {
+      delay: stagger(0.05),
+      duration: 0.1,
+      // easing: [0.17, 0.55, 0.55, 1],
+    }
+  );
+
+  return () => animate(element, { opacity: 0, x: -100 });
+});
