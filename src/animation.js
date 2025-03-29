77c6609edaf426df7934975744d0591640db1f2a
SpectralGT <atharvsingh1396@gmail.com>
2025-03-29 22:22:36 +0530

    added cursor follow object

--- src/animation.js
+++ src/animation.js
@@ -1,4 +1,4 @@
-import { animate, inView, stagger } from "motion";
+import { animate, inView, stagger,motionValue,frame } from "motion";
 
 inView("section h2", (element) => {
   animate(
@@ -58,3 +58,43 @@ inView(".timeline-wrapper", (element) => {
 
   return () => animate(element, { opacity: 1, scale: 1 });
 });
+
+
+const ball = document.querySelector(".ball")
+
+const { top, left, width, height } = ball.getBoundingClientRect()
+const initialX = left + width / 2
+const initialY = top + height / 2
+
+const pointerX = motionValue(0)
+const pointerY = motionValue(0)
+
+function springToPointer() {
+  animate(
+    ball,
+    {
+      x: pointerX.get() - initialX,
+      y: pointerY.get() - initialY,
+    },
+    { type: "spring", stiffness: 100, damping: 10 }
+  )
+}
+
+function scheduleSpringToPointer() {
+  /**
+   * By using `frame.postRender`, we achieve two things:
+   * 1. The animation will be triggered at the end of the animation loop, giving
+   *    any existing spring animations a chance to run for a frame and render.
+   * 2. Debounce the animation to prevent a new one being triggered every pointer
+   *    move, which could be more regular than the animation loop.
+   */
+  frame.postRender(springToPointer)
+}
+
+pointerX.on("change", scheduleSpringToPointer)
+pointerY.on("change", scheduleSpringToPointer)
+
+document.addEventListener("pointermove", (e) => {
+  pointerX.set(e.clientX)
+  pointerY.set(e.clientY)
+})
